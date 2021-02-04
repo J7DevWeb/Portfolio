c61780a22ed3587d9d96f5b079fc0ea25bdf2c67
J7DevWeb <73040578+J7DevWeb@users.noreply.github.com>
2021-02-04 17:14:33 +0100

    Update main.js
    Portfolio V2

--- main.js
+++ main.js
@@ -1,80 +1,37 @@
-// Menu
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+const cv = document.getElementById('cv');
+const front = document.getElementById('front');
+const back = document.getElementById('back');
+const bdd = document.getElementById('bdd');
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
+window.onload = gotoCV();
 
-function show(){
-    mainMenu.style.display ='flex';
-    mainMenu.style.top ='0';
-}
-
-function close(){
-    mainMenu.style.display ='flex';
-    mainMenu.style.top ='-100%';
-}
-
-//Rain
-setInterval(rainFall, 42);
-
-function rainFall() {
-  const waterDrop = document.createElement('i');
-  
-	waterDrop.classList.add('fas');
-	waterDrop.classList.add('fa-tint');
-	waterDrop.style.left = Math.random() * window.innerWidth + 'px';
-	waterDrop.style.animationDuration = Math.random() * 2 + 's';
-	waterDrop.style.opacity = Math.random() + 0.3;
-	waterDrop.style.fontSize = Math.random() * 14 +'px';
-	
-	document.body.appendChild(waterDrop);
-	
-	setTimeout(() => {
-		waterDrop.remove();
-	}, 10000)
+function gotoCV(){
+    cv.style.display = 'block';
+    front.style.display = 'none';
+    back.style.display = 'none';
+    bdd.style.display = 'none';
 };
 
-//title
+function gotoFront(){
+    cv.style.display = 'none';
+    front.style.display = 'block';
+    back.style.display = 'none';
+    bdd.style.display = 'none';
+};
 
-const title = document.querySelector('.title');
-const strTitle = title.textContent;
-const splitTitle = strTitle.split("");
+function gotoBack(){
+    cv.style.display = 'none';
+    front.style.display = 'none';
+    back.style.display = 'block';
+    bdd.style.display = 'none';
+};
 
-title.textContent="";
-for(let i=0; i<splitTitle.length; i++){
-    title.innerHTML += "<span>" + splitTitle[i] + "</span>";
-}
-
-let char = 0;
-let timer = setInterval(onTick,50);
-
-function onTick(){
-    const span = title.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char++
-    if(char ===splitTitle.length){
-        complete();
-        return;
-    }
-
-}
-
-function complete(){
-    clearInterval(timer);
-    timer =null;
-}
-
-
-
-
-
-
-
-
-
-
+function gotoBdd(){
+    cv.style.display = 'none';
+    front.style.display = 'none';
+    back.style.display = 'none';
+    bdd.style.display = 'block';
+};
 
 
 
